71e80840ba4160b3ebe3eba8b7d4832bb4d06d10
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2022-12-10 17:31:18 +0100

    fix

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const router = express.Router();
 const connectDB = require("./config/connectDB");
 connectDB();
 const port = 5500;
-router.get("/", (req, res) => {
+app.use("/", (req, res) => {
   res.json({
     message: "hosting good",
   });
 });
 
-router.get("/all", async (req, res) => {
-  const users = await User.find();
-  res.json(users);
-});
+// router.get("/all", async (req, res) => {
+//   const users = await User.find();
+//   res.json(users);
+// });
 
 app.listen(port, (err) => {
   err
