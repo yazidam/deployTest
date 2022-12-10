b3e81cfcf73ad48586f725cf074875162e08b862
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2022-12-10 16:11:50 +0100

    first commit

---
+++ src/api.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const User = require("../models/User");
+
+const serverless = require("serverless-http");
+
+const app = express();
+
+const router = express.Router();
+
+const connectDB = require("../config/connectDB");
+connectDB();
+const port = 5500;
+router.get("/", (req, res) => {
+  res.json({
+    message: "hosting good",
+  });
+});
+
+router.get("/all", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+app.use("/.netlify/functions/api", router);
+
+app.listen(port, (err) => {
+  err
+    ? console.log("erroor", err)
+    : console.log(`this server is running on ${port}`);
+});
+module.exports.handler = serverless(app);
